fd36d656a1fe343ea8b927c16acbc53a6a9c2a9e
JeongSeon-Lee <ljs658794@gmail.com>
2022-06-06 16:36:02 +0900

    한글 깨지는 지 확인

--- 02/P2-9.cpp
+++ 02/P2-9.cpp
@@ -7,9 +7,9 @@ int main(void)
     int sum = 0;
 
     for (int i = 0; i < 100; i++)
-        sum+= (i + 1);
+        sum += (i + 1);
 
-    cout << "합계:" << sum << endl;
+    cout << "합계 : " << sum << endl;
     cout << i << endl;      // 5로 출력됨
 
     return 0;
