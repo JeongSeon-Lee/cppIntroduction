f29dbe6ca3ee9b65f17bf356aced97b6be0bc08a
JeongSeon-Lee <ljs658794@gmail.com>
2022-06-06 14:33:21 +0900

    (퍼옴 -설명) 2강 연습문제 2-5

--- 02/P2-5.cpp
+++ 02/P2-5.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 using namespace std;
  
-int Sum(int num1 = 0, int num2 = 0, int num3 = 0, int num4 = 0, int num5 = 0)
+int Sum(int num1 = 0, int num2 = 0, int num3 = 0, int num4 = 0, int num5 = 0)   // 전부 디폴트 매개 변수
 {
     return num1 + num2 + num3 + num4 + num5;
 }
  
 int main(void)
 {
-    cout << "Sum():" << Sum() << endl;
+    cout << "Sum():" << Sum() << endl;      // 인수를 안 줄 수도 있음
     cout << "Sum(5):" << Sum(5) << endl;
     cout << "Sum(4, 5):" << Sum(4, 5) << endl;
     cout << "Sum(3, 4, 5):" << Sum(3, 4, 5) << endl;
