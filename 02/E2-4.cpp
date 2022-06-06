d4f86bd9d12ce719e704ce74bf78250d456ee5be
JeongSeon-Lee <ljs658794@gmail.com>
2022-06-06 12:42:35 +0900

    2강 예제 2-4

---
+++ 02/E2-4.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+namespace Microsoft {
+    int g_MVar;
+    int Plus(int x, int y)
+    {
+        return (x + y);
+    }
+    int Minus(int x, int y)
+    {
+        return (x - y);
+    }
+}
+
+namespace Samsung {
+    int g_SVar;
+    int Plus(int x, int y) 
+    {
+        return (x + y);
+    }
+    int Minus(int x, int y);
+}
+
+int Samsung::Minus(int x, int y)
+{
+    return (x - y);
+}
+
+int main(void)
+{
+    Microsoft::g_MVar = Microsoft::Minus(5, 2);
+    cout << Microsoft::g_MVar << endl;
+
+    return 0;
+}
