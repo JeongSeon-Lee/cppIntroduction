da80a02c4b2961f35bdee11a92dfc58e823ba0ac
JeongSeon-Lee <ljs658794@gmail.com>
2022-06-06 16:20:03 +0900

    (부가) 연습문제 2-9

--- 02/P2-9.cpp
+++ 02/P2-9.cpp
@@ -3,12 +3,14 @@ using namespace std;
  
 int main(void)
 {
+    int i = 5;
     int sum = 0;
 
     for (int i = 0; i < 100; i++)
         sum+= (i + 1);
 
     cout << "합계:" << sum << endl;
+    cout << i << endl;      // 5로 출력됨
 
     return 0;
 }
