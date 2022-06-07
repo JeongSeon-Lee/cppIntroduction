78d6231c81355af4c2888cad78186f93d92b5707
JeongSeon-Lee <ljs658794@gmail.com>
2022-06-07 11:25:33 +0900

    (고침) 연습문제 4-4

--- 04/p4-4.cpp
+++ 04/p4-4.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 
 class CStudent {
@@ -8,25 +9,25 @@ private:
     int age;
 
 public:
-    CStudent(char str[20] = "noname", int num = 0, int a = 0) {
+    CStudent(const char *str = "noname", int num = 0, int a = 0) {
         strcpy(name, str); number = num; age = a; }
-    void SetName(char str[20]) { strcpy(name, str); }
+    void SetName(const char *str) { strcpy(name, str); }
     void SetNumber(int num) { number = num; }
     void SetAge(int a) { age = a; }
-    void Print() { cout << "ì´ë¦„ : " << name << endl
-                        << "í•™ë²ˆ : " << number << endl
-                        << "ë‚˜ì´ : " << age << endl; }
+    void Print() { cout << "ÀÌ¸§ : " << name << endl
+                        << "ÇÐ¹ø : " << number << endl
+                        << "³ªÀÌ : " << age << endl; }
 };
 
 int main(void)
 {
-    CStudent St1("í™ê¸¸ë™", 11111111, 25);
+    CStudent St1("È«±æµ¿", 11111111, 25);
     CStudent St2;
 
     St1.Print();
     St2.Print();
 
-    St2.SetName("ì´ìˆœì‹ ");
+    St2.SetName("ÀÌ¼ø½Å");
     St2.SetNumber(22222222);
     St2.SetAge(30);
 
