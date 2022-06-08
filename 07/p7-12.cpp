7e9313d7bd85353d4baa8b09ced9c19bbef9b27e
JeongSeon-Lee <ljs658794@gmail.com>
2022-06-09 00:44:56 +0900

    (고침, 설명) 연습문제 7-12

--- 07/p7-12.cpp
+++ 07/p7-12.cpp
@@ -15,6 +15,12 @@ public :
             ary[i] = rand() % 10;
     }
     ~CArray() { delete [] ary; }
+    CArray(const CArray &Ary) {
+        count = Ary.count;
+        ary = new int[count];
+        for (int i = 0; i < count; i++)
+            ary[i] = Ary.ary[i];
+    }
     CArray &operator=(const CArray &Ary) { 
         count = Ary.count;
         delete [] ary;
@@ -25,7 +31,7 @@ public :
     }
 
     friend ostream &operator<<(ostream &out, CArray &Ary);
-    friend CArray &operator+(const CArray &A1, const CArray &A2);
+    friend CArray operator+(const CArray &A1, const CArray &A2);
 };
 
 ostream &operator<<(ostream &out, CArray &Ary)
@@ -35,17 +41,17 @@ ostream &operator<<(ostream &out, CArray &Ary)
     return out;
 }
 
-CArray &operator+(const CArray &A1, const CArray &A2)
+CArray operator+(const CArray &A1, const CArray &A2)
 {
     CArray A(A1.count + A2.count);
-    int idx;
+    int idx = 0;
 
     for (int i = 0; i < A1.count; i++)
         A.ary[idx++] = A1.ary[i];
     for (int i = 0; i < A2.count; i++)
         A.ary[idx++] = A2.ary[i];
 
-    return A;
+    return A;   // 지역변수를 참조 반환하면 안됨, 지역 객체값 반환, 임시 객체 복사 생성(복사 생성자 필요)
 }
 
 int main(void)
@@ -60,7 +66,7 @@ int main(void)
     cout << Ary2 << endl;
     cout << Ary3 << endl;
 
-    Ary3 = Ary1 + Ary2;
+    Ary3 = Ary1 + Ary2;     // 임시 객체와 Ary3 객체는 무관, 대입과 동시에 임시 객체 소멸
 
     cout << Ary1 << endl;
     cout << Ary2 << endl;
